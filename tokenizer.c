dfb6b8f3c502b436f3637cbb079fe63701d2203c
YamasouA <akiakiskyhand@gmail.com>
2024-02-15 23:29:17 +0900

    ファイル分割

---
+++ tokenizer.c
@@ -0,0 +1,88 @@
+#include "9cc.h"
+
+static bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+static bool startswith(char *p, char *q) {
+  return memcmp(p, q, strlen(q)) == 0;
+}
+
+static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+// 次のトークンが期待している記号の時はトークンを1つ読み進めてtrueを返す
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED || 
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号の時はトークンを1つ読み進める
+// それ以外ならエラーを報告する
+void expect(char *op) {
+  if (token-> kind != TK_RESERVED || 
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    error_at(token->str, "'%s'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合トークンを1つ読み進めてその数値を返す
+// それ以外の場合にはエラーを報告する
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+Token *tokenize() {
+  char *p = user_input;
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (startswith(p, "==") || startswith(p, "!=")
+        || startswith(p, ">=") || startswith(p, "<=")) {
+          cur = new_token(TK_RESERVED, cur, p, 2);
+          p += 2;
+          continue;
+    }
+
+    if (strchr("+-*/()<>", *p)) {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
