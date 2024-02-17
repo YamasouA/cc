46c72324be66cdd47eca1fd5a895f9cb6735e78a
YamasouA <akiakiskyhand@gmail.com>
2024-02-17 12:52:01 +0900

    else文のテストの追加

--- test.sh
+++ test.sh
@@ -60,4 +60,6 @@ assert 3 'if (0) return 2; return 3;'
 assert 3 'if (1-1) return 2; return 3;'
 assert 2 'if (1) return 2; return 3;'
 assert 2 'if (2-1) return 2; return 3;'
+assert 2 'if (2-1) a = 2; else a = 3; return a;'
+assert 3 'if (0) a = 2; else a = 3; return a;'
 echo OK
